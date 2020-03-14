4f96e50511c78c33d73a137e21400254c7cfb1fc
Jianlong <damonhuang233@gmail.com>
2020-03-14 01:55:13 -0700

    finish UI set up and types page

---
+++ app/src/main/java/com/example/chooseyourmeal/data/ListTypeItem.java
@@ -0,0 +1,11 @@
+package com.example.chooseyourmeal.data;
+
+public class ListTypeItem {
+    public String Type;
+    public String TypeImageURL;
+
+    public ListTypeItem(String type, String url){
+        Type = type;
+        TypeImageURL = url;
+    }
+}
