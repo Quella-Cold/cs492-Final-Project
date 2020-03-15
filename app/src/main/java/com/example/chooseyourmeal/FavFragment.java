dde7200d609ca1020425e014adea0601b793c5c8
Csedge <liangfen@oregonstate.edu>
2020-03-15 15:38:55 -0700

    1

--- app/src/main/java/com/example/chooseyourmeal/FavFragment.java
+++ app/src/main/java/com/example/chooseyourmeal/FavFragment.java
@@ -14,5 +14,6 @@ public class FavFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_fav, container, false);
+
     }
 }
